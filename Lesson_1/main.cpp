a7de0ec08d78407c91f6680309067c58c3025b3c
Uteshev Maksim <guitarheroe2012@ya.ru>
2025-05-27 14:55:35 +0300

    data types

---
+++ Lesson_1/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+
+/* int main() {
+    const double PI =3.1445;
+    unsigned long long result = 1;
+    int i;
+    for (int i = 0; i <= 64; ++i) {
+        result *= 2;
+    }
+    std::cout << (result - 1) / (1024 * 1024)<<"\n";
+    std::cout << sizeof(i);
+    return 0;
+} */
+int main()
+{
+long i = 28l;
+double n = 2E+6L;
+int r  = 32l;
+std::cout << i << "\n";
+std::cout << 057 << "\n";
+std::cout << 0x1A << "\n";
+std::cout << n << "\n";
+std::cout << r << "\n";
+std::cout << 2.148E3 << "\n";
+}
